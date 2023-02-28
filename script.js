684c176999bb90a69c0f5e4e38ed727c078d022b
katadjina <94002120+katadjina@users.noreply.github.com>
2023-02-28 23:16:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const input = document.querySelector('.insert');
 const add = document.querySelector('.add');
-const filterOption = document.querySelector('.topics');
+const filter = document.querySelector('.topics');
 const todoList = document.querySelector('.todo-list');
 
 
 document.addEventListener('DOMContentLoaded',getTodos);
 add.addEventListener('click',setTodo);
 todoList.addEventListener('click',action);
-filterOption.addEventListener('input',filterTodo);
+filter.addEventListener('input',filterTodo);
 
 
 
